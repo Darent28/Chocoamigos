743582c8c349ff0ca5a89d7416b1f4525969bcbc
Darent28 <davidantonioelizondoramos@gmail.com>
2022-07-25 13:42:40 -0500

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+    @about_me = "My Name is David Elizondo, Im 3D Artist and Programmer in my free time."
+  end
+  
+  def _header
+  end
+
+  def addamigo
+  end
+
+end
